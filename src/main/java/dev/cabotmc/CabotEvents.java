ba6c77cfba9fc8222372b5e3f392e2076aa609b2
Kyle Smith <kyle@cabotmc.dev>
2024-10-05 16:58:43 -0300

    Add no-collision team

--- src/main/java/dev/cabotmc/CabotEvents.java
+++ src/main/java/dev/cabotmc/CabotEvents.java
@@ -3,20 +3,33 @@ package dev.cabotmc;
 import org.bukkit.Bukkit;
 import org.bukkit.GameRule;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scoreboard.Team;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 public class CabotEvents implements Listener {
+
+    public static Team playersTeam = null;
 
     @EventHandler
     public void spawn(PlayerSpawnLocationEvent event) {
         if (event.getPlayer().getFirstPlayed() == event.getPlayer().getLastLogin()) {
             // first spawn
             event.setSpawnLocation(new Location(Bukkit.getWorld("world"), 0, 65, 0));
+            playersTeam.addPlayer(event.getPlayer());
+
+            event.getPlayer().getInventory().addItem(
+                    new ItemStack(Material.COOKED_BEEF, 16),
+                    new ItemStack(Material.WHITE_BED),
+                    new ItemStack(Material.OAK_SLAB, 16)
+            );
         }
     }
     @EventHandler
@@ -24,4 +37,16 @@ public class CabotEvents implements Listener {
         event.getWorld().setGameRule(GameRule.SPAWN_RADIUS, 0);
         event.getWorld().setGameRule(GameRule.PLAYERS_SLEEPING_PERCENTAGE, 1);
     }
+
+    @EventHandler
+    public void ready(ServerLoadEvent e) {
+        playersTeam = Bukkit.getServer().getScoreboardManager().getMainScoreboard().getTeam("players");
+        if (playersTeam == null) {
+            playersTeam = Bukkit.getServer().getScoreboardManager().getMainScoreboard().registerNewTeam("players");
+            playersTeam.setAllowFriendlyFire(false);
+            playersTeam.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
+        }
+    }
+
+
 }
--- src/main/java/dev/cabotmc/Main.java
+++ src/main/java/dev/cabotmc/Main.java
@@ -2,17 +2,21 @@ package dev.cabotmc;
 
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Main extends JavaPlugin
 {
+
+
     @Override
     public void onEnable() {
         getLogger().info("Hello");
 
         // tell the server to use BenryEvents to check for events
         getServer().getPluginManager().registerEvents(new CabotEvents(), this);
+
     }
 
     @Override
